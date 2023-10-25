c1e674ec633669e8573918c08b726c588d71d921
nodirbekomonboyev <omonboevnodir77@gmail.com>
2023-10-25 16:55:20 +0500

    Initial commit

---
+++ src/test/java/uz/problemservice/ProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.problemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
